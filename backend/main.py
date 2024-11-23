9d95a60db9e52c5c211893bd56f6e88ab1cdb94a
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-11-22 17:44:29 -0800

    GET and POST routes

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,23 @@ def get_tasks():
     json_tasks = list(map(lambda task: task.to_json(), tasks))
     return jsonify({"tasks": json_tasks})
 
+@app.route("/create_task", methods=["POST"])
+def create_task():
+    date = request.json.get("date")
+    task = request.json.get("task")
+
+    if not date or not task:
+        return (jsonify({"msg": "You must include date and task"}), 400)
+    
+    new_task = Todo(date=date, task=task)
+
+    try:
+        db.session.add(new_task)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"msg": str(e)}, 400)
+    
+    return jsonify({"msg": "Task created successfully"}, 201)
 
 if __name__ == '__main__':
     with app.app_context():
